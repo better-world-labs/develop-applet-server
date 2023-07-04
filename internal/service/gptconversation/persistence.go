ff60d0000888c4de2c9675c8a7e0c7f5e449c1e8
shenweijie <shenweijie@openviewtech.com>
2023-07-04 10:24:53 +0800

    feat: init conversation greet

--- internal/service/gptconversation/persistence.go
+++ internal/service/gptconversation/persistence.go
@@ -40,6 +40,6 @@ func (p *persistence) pageByUserId(query page.StreamQuery, userId int64) (*page.
 }
 
 func (p *persistence) isEmptyConversation(userId int64) (bool, error) {
-	has, err := p.Where("user_id = ?", userId).Exist(entity.GptChatMessage{})
+	has, err := p.Where("user_id = ?", userId).Exist(&entity.GptChatMessage{})
 	return !has, err
 }
